e82474489bf66a09b9b31921e296d943ce3c2e6f
Prozhikin_Andrey <a.prozhikin@gmail.com>
2024-02-11 14:25:56 +0200

    add Task1 into repository

---
+++ src/test/java/ru/geekbrains/SpringHW4/SpringHw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SpringHW4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
